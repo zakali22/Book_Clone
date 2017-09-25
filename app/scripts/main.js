c7f78dc9f335237ee10fdee9da117caa5e45a786
Zakeria Ali <zak.fined@gmail.com>
2017-09-25 01:30:18 +0100

    Design the navigation bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.browse').on('mouseover', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    $('.cats').css('display', 'inline-block');
+  });
+
+});
